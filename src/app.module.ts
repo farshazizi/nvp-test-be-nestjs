e1e5972b39f692fb7e6151d08021ada97742c342
farshazizi <farshazizi22@gmail.com>
2025-12-05 00:01:33 +0700

    feat: crud employee management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nvp_test',
+      autoLoadEntities: true,
+      synchronize: true, // DEV only
+    }),
+    EmployeeModule,
+  ],
 })
 export class AppModule {}
